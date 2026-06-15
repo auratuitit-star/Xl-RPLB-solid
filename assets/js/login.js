e27a1c9d1d8be3d2214ebeb375f38576a8362e29
Sevira1209-oos <auratuitit@gmail.com>
2026-06-15 23:57:32 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,39 +16,23 @@ window.onload = function () {
             .value
             .trim();
 
-        console.log("Username:", username);
-        console.log("Password input:", password);
-        console.log("Data user:", users[username]);
-
         if (users[username]) {
-
-            console.log("Password users.js:", users[username].password);
-            console.log(
-                "Hasil perbandingan:",
-                users[username].password === password
-            );
 
             if (users[username].password === password) {
 
                 alert("Login berhasil!");
 
                 localStorage.setItem(
-                    "currentUser",
+                    "userLogin",
                     JSON.stringify(users[username])
                 );
-                localStorage.setItem(
 
-    "userLogin",
-
-    JSON.stringify(user)
-
-);
-
-                window.location.href = "dashboard.html";
+                window.location.href =
+                    "dashboard.html";
 
             } else {
 
-                alert("Password tidak cocok!");
+                alert("Password salah!");
 
             }
 
